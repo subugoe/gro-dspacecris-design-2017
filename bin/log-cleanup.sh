61564ad4e7f478ff06aa981bfd521fbdb15264ff
Nils Windisch <nils@windisch.me>
2018-01-04 11:40:35 +0100

    REMOVE obsolete workflow files

--- bin/log-cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC1091
-source ./bin/lib.sh
-
-rm ./log.log
-touch log.log
-
-log "🗑 Log Cleanup"
--- bin/npm-check-updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC1091
-source ./bin/lib.sh
-
-log "🛠 NPM Check Updates"
-
-./node_modules/.bin/ncu
